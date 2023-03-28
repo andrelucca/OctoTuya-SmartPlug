ae76a8cfb1db374322443576396e10590fd84f75
Andre Lucca <andlucca@hotmail.com>
2023-03-27 21:21:01 -0300

    fix: post process script dynamic commands

--- ultimaker_cura_script/TurnOffPrinter.py
+++ ultimaker_cura_script/TurnOffPrinter.py
@@ -32,17 +32,28 @@ class TurnOffPrinter(Script):
                     "description": "The configured label in the Octotuya-SmartPlug Octoprint plugin",
                     "type": "str",
                     "default_value": ""
+                },
+                "command": {
+                    "label": "Command",
+                    "description": "GCODE command to turn off the printer",
+                    "type": "enum",
+                    "options": {
+                        "M81": "M81",
+                        "G4 P2": "G4 P2"
+                    },
+                    "default_value": "M81"
                 }
             }
         }"""
 
     def execute(self, data):
         label_string = self.getSettingValueByKey("label")
+        command = self.getSettingValueByKey("command")
         if self.getSettingValueByKey("enabled"):
             
             seach_string_escape = re.escape(";End of Gcode")
             seach_string_compile = re.compile(seach_string_escape)
-            turn_off_string = 'M81 '+label_string+';Turn off printer using OctoTuya\n;End of Gcode'
+            turn_off_string = command+' '+label_string+' ;Turn off printer using OctoTuya\n;End of Gcode'
 
             for layer_number, layer in enumerate(data):
                 data[layer_number] = re.sub(seach_string_compile, turn_off_string, layer)
